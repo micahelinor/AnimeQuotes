f500d2e8039d55eece652620ca4ee979f75302db
micahelinor <mferas@berkeley.edu>
2020-09-21 20:42:03 -0700

    codebase RiskQ rampup

---
+++ src/test/java/AnimeQuotes/AnimeQuotesApplicationTests.java
@@ -0,0 +1,13 @@
+package AnimeQuotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeQuotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
