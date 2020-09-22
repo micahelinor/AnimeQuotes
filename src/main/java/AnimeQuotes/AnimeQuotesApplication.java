6006d1d9abde7fd6af1a30aa708200f1c5f1e6fe
micahelinor <mferas@berkeley.edu>
2020-09-21 23:23:49 -0700

    parsed json

--- src/main/java/AnimeQuotes/AnimeQuotesApplication.java
+++ src/main/java/AnimeQuotes/AnimeQuotesApplication.java
@@ -1,23 +1,26 @@
 package AnimeQuotes;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+
 @SpringBootApplication
 @RestController
-public class AnimeQuotesApplication<input, quote> {
+public class AnimeQuotesApplication<quote> {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AnimeQuotesApplication.class, args);
 	}
 
 	@GetMapping("/hello")
-	public void hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+	public void hello(@RequestParam(value = "name", defaultValue = "World") String name) throws JsonProcessingException, JSONException {
 		// request url
 		String url = "https://animechanapi.xyz/api/quotes/random";
 
@@ -27,7 +30,14 @@ public class AnimeQuotesApplication<input, quote> {
 		// make an HTTP GET request
 		String json = restTemplate.getForObject(url, String.class);
 
-		// print json
-		System.out.println(json);
+		//Converting info from API call to JSON object and formatting.
+		JSONObject response = new JSONObject(json);
+		String quote = response.getJSONArray("data").getJSONObject(0).getString("quote");
+		String character = response.getJSONArray("data").getJSONObject(0).getString("character");
+
+		System.out.println(quote);
+		System.out.println(character);
+		//return String.format("Hello %s!", name);
 	}
+
 }
