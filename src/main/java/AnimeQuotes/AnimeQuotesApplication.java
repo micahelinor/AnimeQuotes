b0581c7931c777c8c0ec9e48e205f0e06804bc98
micahelinor <mferas@berkeley.edu>
2020-09-21 21:02:02 -0700

    made more changes

--- src/main/java/AnimeQuotes/AnimeQuotesApplication.java
+++ src/main/java/AnimeQuotes/AnimeQuotesApplication.java
@@ -7,9 +7,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.io.*;
+import org.json.*;
+
 @SpringBootApplication
 @RestController
 public class AnimeQuotesApplication {
+
+	String mainUrl;
+	public AnimeQuotesApplication() {
+		mainUrl = "https://animechanapi.xyz/api/quotes/random";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AnimeQuotesApplication.class, args);
@@ -19,4 +29,12 @@ public class AnimeQuotesApplication {
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
 	}
+
+	//Converting to URL
+	URL url = new URL(newURL.toString());
+
+	//Opening API Connection and making it a GET Request
+	HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+		connection.setRequestMethod("GET");
+		
 }
