30fcb13dbe3b62a98ff03dcb04596b88ff67c506
micahelinor <mferas@berkeley.edu>
2020-09-21 22:29:40 -0700

    got the api call running

--- src/main/java/AnimeQuotes/AnimeQuotesApplication.java
+++ src/main/java/AnimeQuotes/AnimeQuotesApplication.java
@@ -6,35 +6,28 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.io.*;
-import org.json.*;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RestController
-public class AnimeQuotesApplication {
-
-	String mainUrl;
-	public AnimeQuotesApplication() {
-		mainUrl = "https://animechanapi.xyz/api/quotes/random";
-	}
+public class AnimeQuotesApplication<input, quote> {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AnimeQuotesApplication.class, args);
 	}
 
 	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
+	public void hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		// request url
+		String url = "https://animechanapi.xyz/api/quotes/random";
+
+		// create an instance of RestTemplate
+		RestTemplate restTemplate = new RestTemplate();
+
+		// make an HTTP GET request
+		String json = restTemplate.getForObject(url, String.class);
+
+		// print json
+		System.out.println(json);
 	}
-
-	//Converting to URL
-	URL url = new URL(newURL.toString());
-
-	//Opening API Connection and making it a GET Request
-	HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-		connection.setRequestMethod("GET");
-		
 }
